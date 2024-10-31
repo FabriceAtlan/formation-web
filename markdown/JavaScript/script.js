ad9051b93d8b298011cc63eb1d6b59d953260333
Fabrice Atlan <fabrice.atlan.56@gmail.com>
2024-10-31 17:21:40 +0100

    Update

--- markdown/JavaScript/script.js
+++
@@ -1,8 +0,0 @@
-array = [6, 2, 1, 8, 10];
-console.log(sumArray(array));
-
-function sumArray(array) {
-  const min = array.reduce((acc, val, id) => Math.min(acc, val), Infinity);
-  const max = array.reduce((acc, val, id) => Math.max(acc, val), -Infinity);
-  console.log(min, max);
-}
